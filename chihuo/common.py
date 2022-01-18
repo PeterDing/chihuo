0143f29858984832e16908734f575ad4db123995
PeterDing <dfhayst@gmail.com>
2022-01-18 16:20:22 +0800

    Change `ChihuoType` name as `TaskType`; Use `Enum` for `TaskType`

--- chihuo/common.py
+++ chihuo/common.py
@@ -1,14 +1,20 @@
+from typing import Union
+from enum import Enum
+
 SERVER_DEFAULT_CONFIG_PATH = "server.config"
 
-
-class ChihuoType:
-    Task = 1
-    Start = 2
-    TaskLoop = 3
-    SendBack = 4
-    Handler = 5
+TaskId = Union[str, bytes]
 
 
-class Direction:
+class TaskType(Enum):
+    Task = "task"
+    Start = "start"
+    TaskLoop = "task_loop"
+    SendBack = "send_back"
+    Handler = "handler"
+    Final = "final"
+
+
+class Direction(Enum):
     Forward = 0
     Reverse = 1
