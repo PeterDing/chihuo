4bf32cd5706031c43f81a7c20d67102ca58b2d21
PeterDing <dfhayst@gmail.com>
2022-01-18 16:27:00 +0800

    Handle INT and TERM signals as following steps:
    1. Set `factory.stop` to True
    2. Cancel all running tasks except the `TaskType.Final` and
       `TaskType.TaskLoop` tasks.
    3. Cancel all `TaskType.TaskLoop` tasks except the `TaskType.Final`
    4. Stop the loop

--- chihuo/signal_handlers.py
+++ chihuo/signal_handlers.py
@@ -2,8 +2,10 @@ import logging
 import asyncio
 import functools
 import signal
+from typing import Iterable
 
-from .common import ChihuoType
+from .common import TaskType, Direction
+from .loop import ChihuoLoop
 
 logger = logging.getLogger(__name__)
 
@@ -11,70 +13,96 @@ IS_ON_TERM = False
 IS_ON_INT = False
 
 
-async def _wait_for_tasks(loop):
-    logger.info("wait for futures to completed")
+async def cancel_all_running_tasks(loop, factories: Iterable[ChihuoLoop] = []) -> None:
+    """Cancel all running tasks, then send these tasks back to backend"""
 
     while True:
-        tasks = asyncio.all_tasks(loop)
-        logger.warning("current tasks: %s", len(tasks))
-        if len(tasks) <= 1:
-            break
-        await asyncio.sleep(1, loop)
-        cancel_nonchihuo_tasks(loop)
+        # Cancel all running async tasks in the loop
+        tasks = [
+            t for t in asyncio.all_tasks(loop) if t.get_name() != TaskType.Final.value
+        ]
+
+        logger.info("all tasks number: %s", len(tasks))
+
+        task_loop = [t for t in tasks if t.get_name() == TaskType.TaskLoop.value]
+        needed_cancel = [t for t in tasks if t.get_name() != TaskType.TaskLoop.value]
+
+        for task in needed_cancel:
+            task.cancel()
+
+        if len(needed_cancel) == 0:
+            # Finally, cancel the `ChihuoLoop._task_loop`
+            if len(task_loop) == 0:
+                break
+            else:
+                for task in task_loop:
+                    task.cancel()
+
+        await asyncio.sleep(1)
+
+    await sendback_tasks(factories)
 
     loop.stop()
 
 
-def cancel_nonchihuo_tasks(loop):
-    tasks = asyncio.all_tasks(loop)
-    for task in tasks:
-        if not hasattr(task, "ch_type"):
-            task.cancel()
+async def sendback_tasks(factories: Iterable[ChihuoLoop]) -> None:
+    """Send all tasks in `ChihuoLoop._running_tasks` to backend"""
+
+    for factory in factories:
+        factory_name = factory.__class__.__name__
+        pairs = list(factory._running_tasks.items())
+        logger.info("%s: sendback tasks", factory_name)
+        logger.info("%s: sendback_task: tasks: %s", factory_name, pairs)
+
+        for pair in pairs:
+            await factory.add_task(pair, direction=Direction.Reverse)
+
+        logger.info("%s: sendback_task has done", factory_name)
 
 
-def wait_for_tasks(signum, frame, factories=None, loop=None):
-    logger.info("handle signal %s: wait_for_tasks", signum)
+def handle_stop_and_send(
+    signum,
+    frame,
+    factories: Iterable[ChihuoLoop] = [],
+    loop: asyncio.AbstractEventLoop = None,
+) -> None:
+    """The handler of INT and TERM signals
+
+    First, we set all factories to be stoped at next event loop.
+    Then, we cancel all running tasks and send them to backend.
+    """
+
+    assert loop, "The Event Loop can not be None"
+
+    logger.info("handle signal %s: handle_stop_and_send", signum)
 
     global IS_ON_TERM
     global IS_ON_INT
     if IS_ON_TERM or IS_ON_INT:
         return
 
+    # Stop the factory task loop
     for factory in factories:
         factory.stop = True
 
-    cancel_nonchihuo_tasks(loop)
-    task = loop.create_task(_wait_for_tasks(loop))
-    task.ch_type = ChihuoType.Handler
-
-    IS_ON_TERM = True
-
-
-def send_back_tasks(signum, frame, factories=None, loop=None):
-    logger.info("handle signal %s: send_back_tasks", signum)
-
-    global IS_ON_TERM
-    global IS_ON_INT
-    if IS_ON_TERM or IS_ON_INT:
-        return
-
-    for factory in factories:
-        factory.stop = True
-        factory._sendback_and_cancel_tasks()
-
-    cancel_nonchihuo_tasks(loop)
-    task = loop.create_task(_wait_for_tasks(loop))
-    task.ch_type = ChihuoType.Handler
+    # Spawn the final task
+    final_task = cancel_all_running_tasks(loop, factories)
+    loop.create_task(final_task, name=TaskType.Final.value)
 
     IS_ON_INT = True
 
 
-def set_signal_handlers(factories, loop):
+def set_signal_handlers(
+    factories: Iterable[ChihuoLoop], loop: asyncio.AbstractEventLoop
+) -> None:
+    """Set signal handlers for `factories`"""
+
     signal.signal(
         signal.SIGTERM,
-        functools.partial(wait_for_tasks, factories=factories, loop=loop),
+        functools.partial(handle_stop_and_send, factories=factories, loop=loop),
+        # functools.partial(wait_for_tasks, factories=factories, loop=loop),
     )
     signal.signal(
         signal.SIGINT,
-        functools.partial(send_back_tasks, factories=factories, loop=loop),
+        functools.partial(handle_stop_and_send, factories=factories, loop=loop),
     )
